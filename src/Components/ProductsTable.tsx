c47cef4666f31e5bc43a7845e304f0965a9675b5
Luan Alves <akaluan.la@gmail.com>
2021-05-09 02:58:17 -0300

    adicionado filtro por nome do produto

--- src/Components/ProductsTable.tsx
+++ src/Components/ProductsTable.tsx
@@ -1,15 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import ProductsContext from '../Utils/ProductsContext'
-import { TableContainer, IconButton } from '@material-ui/core'
+import { TableContainer, IconButton, Grid, TextField } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
 import DataTable from 'react-data-table-component'
 import { Product, ProductFormsProps } from '../Utils/Interfaces'
-
+import { Edit, Search } from '@material-ui/icons'
 
 /* Criação do componente table */
 export function ProductsTable({ handleOpenModal }: ProductFormsProps) {
 
   const { products, handleRemoveProduct } = useContext(ProductsContext)
+  const [ filteredProducts, setFilteredProducts ] = useState<Product[]>([])
 
 
   /* Criação das colunas que são requeridas pelo componente da lib react-data-table-component */
@@ -35,30 +36,53 @@ export function ProductsTable({ handleOpenModal }: ProductFormsProps) {
       sortable: true
     },
     {
-      name: 'Ações',
+      name: 'Ações',      
       cell: (row: Product) => <IconButton aria-label="delete" onClick={() => {
         handleRemoveProduct(row.cod_sku)
         handleOpenModal('Produto removido com sucesso!')
-        }}>
+      }}>
         <DeleteIcon />
+      </IconButton>,
+      button: true
+    },
+    {
+      name: 'Ações',
+      cell: () => <IconButton>
+        <Edit />
       </IconButton>,
       button: true
     }
   ]
 
+  function filterItems(e: string) {
+    const filtered = products.filter(product => {
+      return product.prod_name && product.prod_name.toLowerCase().includes(e.toLowerCase())
+    })
+    setFilteredProducts(filtered)
+  }
+
   return (
     /* Criação da tabela de produtos utilizado um container da materialUI e a table da lib react-data-table-component*/
     <TableContainer>
       <div><h2>Tabela de produtos</h2></div>
+      <Grid container spacing={1} alignItems="flex-end">
+        <Grid item>
+          <Search color="primary"/>
+        </Grid>
+        <Grid item>
+          <TextField id="input-with-icon-grid" label="Procurar pelo nome" onChange={e => filterItems(e.target.value)} />
+        </Grid>
+      </Grid>
       <DataTable
         noHeader
+        noDataComponent={<h3>Sem dados a serem mostrados</h3>}
         defaultSortField="cod_sku"
         defaultSortAsc={false}
         pagination
-        highlightOnHover  
+        highlightOnHover
         title="Tabela de produtos"
         columns={columns}
-        data={products.slice(1)}
+        data={filteredProducts.length === 0 ? products.slice(1) : filteredProducts}
       />
     </TableContainer>
   );
--- src/Styles/GlobalStyle.ts
+++ src/Styles/GlobalStyle.ts
@@ -1,9 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
+  }
+
+  body {
+    font-size: 16px
   }
 
   
